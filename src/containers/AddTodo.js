355199da37711ab0ee42ea7f0d006fb455a0f653
Erik Yael Mora Alcocer <erik.moraa@outlook.com>
2020-07-23 10:22:09 -0500

    AddTodo component is now working

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import { addTodo } from "../actions";
 
-const AddTodo = () => {
-  return <div></div>;
+const AddTodo = (props) => {
+  let input;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.value.trim()) {
+      return;
+    }
+    props.dispatch(addTodo(input.value));
+    input.value = "";
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={(el) => (input = el)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default connect()(AddTodo);
