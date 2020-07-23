ae5d49df662bde59673714a20f94848e5a8fd0c2
Erik Yael Mora Alcocer <erik.moraa@outlook.com>
2020-07-23 09:12:39 -0500

    create AddTodo component template

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import AddTodo from "./containers/AddTodo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Redux Kata</h1>
+        <AddTodo />
       </header>
-        <strong><i>TodoList</i> with redux.</strong>
-        <hr/>
-        <p>This app should be able to:</p>
-        <ol>
-            <li>input text and a button to Add list items</li>
-            <li>Buttons to show/hide list</li>
-            <li>Buttons to show/hide completed/active tasks</li>
-            <li>Add/Remove list items</li>
-            <li>Mark items as completed (use checkbox)</li>
-            <li>Reset list</li>
-        </ol>
-        <hr/>
-        <p>General guidelines</p>
-        <ol>
-            <li>Install react-redux</li>
-            <li>Create an actions folder with an index.js file that handles redux actions (with types)</li>
-            <li>Use a reducer to handle actions</li>
-            <li>Use a reducer to handle Add/remove show/hide tasks actions</li>
-            <li>Use a reducer to handle show list/hide list actions</li>
-            <li>Use combine reducers</li>
-            <li>Use a different component for each action</li>
-        </ol>
     </div>
   );
 }
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const AddTodo = () => {
+  return <div></div>;
+};
+
+export default connect()(AddTodo);
