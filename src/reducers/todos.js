cf65aa0c7d2e3e3c67966e1d9c5c16ccaa05397a
Erik Yael Mora Alcocer <erik.moraa@outlook.com>
2020-07-23 13:23:11 -0500

    add resetList action into reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -15,6 +15,8 @@ const todos = (state = [], action) => {
       );
     case "DELETE_TODO":
       return state.filter((todo) => todo.id !== action.id);
+    case "RESET_LIST":
+      return (state = []);
     default:
       return state;
   }
